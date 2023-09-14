88478777147a492f520f552ef1f07d4bcec20136
gaodi16366 <gaodi16366@gmail.com>
2023-09-14 20:51:21 +0800

    implement mediaplayer

--- app/src/main/java/com/example/dynamicvolumelullaby/MainActivity.kt
+++ app/src/main/java/com/example/dynamicvolumelullaby/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : ComponentActivity() {
         applyPermission()
         context = this.application
         LoadSoundAndConfig()
-        val audioManager = context!!.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val audioManager = applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         currentVolumeLive.postValue(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC))
 
         setContent {
@@ -97,9 +97,11 @@ fun ButtonAndImage(modifier: Modifier = Modifier){
     val maxVolume by maxVolumeLive.observeAsState()
     val currentVolume by currentVolumeLive.observeAsState()
     val baseFft:Array<Pair<Int,Double>>? by baseFftLive.observeAsState()
-
-    val audioManager = context!!.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-    val volumeIndex = currentVolume!!.toFloat()/audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC).toFloat()
+    var volumeIndex = currentVolume!!/15f
+    if (context !=null){
+        val audioManager = context!!.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        volumeIndex = currentVolume!!.toFloat()/audioManager!!.getStreamMaxVolume(AudioManager.STREAM_MUSIC).toFloat()
+    }
 
     saveConfig()
 
@@ -121,8 +123,14 @@ fun ButtonAndImage(modifier: Modifier = Modifier){
             Button(onClick = { /*TODO*/ }) {
                 Image(painter = painterResource(R.drawable.baseline_play_arrow_24), contentDescription = "Play")
             }
+            Button(onClick = {
+                stopPlaying()
+            })
+            {
+                Image(painter = painterResource(R.drawable.baseline_pause_24), contentDescription = "Play")
+            }
         }
-        Text(text = baseFft!!.joinToString("\n"), color = Orange)
+//        Text(text = baseFft!!.joinToString("\n"), color = Orange)
 
         Spacer(modifier = Modifier.height(40.dp))
 
@@ -131,7 +139,8 @@ fun ButtonAndImage(modifier: Modifier = Modifier){
         Row(){
             Text(text = "basic volume ", color = Orange ,modifier = Modifier
                 .align(Alignment.CenterVertically)
-                .width(100.dp))
+                .width(100.dp)
+                .padding(start = 10.dp))
             Slider(value = basicVolume!!,
                 onValueChange ={
                     var nextBasicVolume =  if(it<minVolume!!){
@@ -148,7 +157,8 @@ fun ButtonAndImage(modifier: Modifier = Modifier){
         Row(){
             Text(text = "min volume", color = Orange ,modifier = Modifier
                 .align(Alignment.CenterVertically)
-                .width(100.dp))
+                .width(100.dp)
+                .padding(start = 10.dp))
             Slider(value = minVolume!!,
                 onValueChange ={ minVolumeLive.postValue( min(it, basicVolume!!)) },
                 modifier = Modifier.padding(end= 10.dp))
@@ -156,7 +166,8 @@ fun ButtonAndImage(modifier: Modifier = Modifier){
         Row(){
             Text(text = "max volume", color = Orange ,modifier = Modifier
                 .align(Alignment.CenterVertically)
-                .width(100.dp))
+                .width(100.dp)
+                .padding(start = 10.dp))
             Slider(value = maxVolume!!,
                 onValueChange ={ maxVolumeLive.postValue( max(it, basicVolume!!))},
                 modifier = Modifier.padding(end= 10.dp))
@@ -166,7 +177,8 @@ fun ButtonAndImage(modifier: Modifier = Modifier){
                 color = Orange ,
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
-                    .width(100.dp))
+                    .width(100.dp)
+                    .padding(start = 10.dp))
             Slider(value = volumeIndex,
                 onValueChange ={ /* nothing */},
                 modifier = Modifier
@@ -184,12 +196,12 @@ fun ButtonAndImage(modifier: Modifier = Modifier){
                 Image(painter = painterResource(R.drawable.baseline_stop_24), contentDescription = "Play")
             }
             Button(onClick = {
-                startMonitoring(typeSoundPaths[RecordType.PARENT])
+                startPlaying(typeSoundPaths[RecordType.PARENT])
             }) {
                 Image(painter = painterResource(R.drawable.baseline_play_arrow_24), contentDescription = "Play")
             }
             Button(onClick = {
-                stopMonitoring()
+                stopPlaying()
             })
             {
                 Image(painter = painterResource(R.drawable.baseline_pause_24), contentDescription = "Play")
@@ -202,16 +214,14 @@ fun ButtonAndImage(modifier: Modifier = Modifier){
         Row(){
             Button(onClick = {
                 recordSound(RecordType.MONITOR)
-                startMonitoring(typeSoundPaths[RecordType.PARENT])
             }) {
                 Image(painter = painterResource(R.drawable.baseline_play_arrow_24), contentDescription = "Play")
             }
             Button(onClick = {
                 stopRecord(RecordType.MONITOR)
-                stopMonitoring()
             })
             {
-                Image(painter = painterResource(R.drawable.baseline_pause_24), contentDescription = "Play")
+                Image(painter = painterResource(R.drawable.baseline_stop_24), contentDescription = "Play")
             }
         }
     }
--- app/src/main/java/com/example/dynamicvolumelullaby/PlayingService.kt
+++ app/src/main/java/com/example/dynamicvolumelullaby/PlayingService.kt
@@ -3,27 +3,42 @@ package com.example.dynamicvolumelullaby
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
 import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.os.IBinder
+import android.os.PowerManager
 import java.io.File
 import java.util.Date
 import java.util.LinkedList
 
-val ACTION_NAME = "action_type"
+const val ACTION_NAME = "action_type"
 
-val PARAM_PATH = "path"
+const val PARAM_PATH = "path"
 
-val ACTION_START = 1
-val ACTION_STOP = 2
+const val ACTION_INVALID =0
+const val ACTION_START = 1
+const val ACTION_STOP = 2
 
 // monitoring data and config
 const val intervalSeconds = 5
 var monitorFftDataSum: DoubleArray = DoubleArray(sampleNumber){0.0}
 var previousMonitorDateTime: Date? = null
 var monitorCount:Int =0
+var playingStatus = PlayingStatus.STOP
 
-class PlayingService:Service() {
+
+enum class PlayingStatus{
+    PLAY,
+    STOP,
+    PAUSE
+}
+
+class PlayingService:Service(), MediaPlayer.OnPreparedListener {
+
+    private var mediaPlayer:MediaPlayer? =null
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -34,8 +49,53 @@ class PlayingService:Service() {
             return super.onStartCommand(null, flags, startId)
         }
         var bundle:Bundle? = intent.extras
+        if (bundle !=null){
+            var action= bundle.getInt(ACTION_NAME, ACTION_INVALID)
+            when(action){
+                ACTION_START -> {
+                    var path=bundle.getString(PARAM_PATH)
+                    var file=File(path)
+                    if (file.exists() && (mediaPlayer ==null || !mediaPlayer!!.isPlaying)){
+                        val myUri: Uri = Uri.fromFile(file) // initialize Uri here
+                        mediaPlayer = MediaPlayer().apply {
+                            setAudioAttributes(
+                                AudioAttributes.Builder()
+                                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                                    .build()
+                            )
+                            setDataSource(applicationContext, myUri)
+                            isLooping = true
+                            prepareAsync()
+                            setWakeMode(applicationContext, PowerManager.PARTIAL_WAKE_LOCK)
+                        }
+                        playingStatus = PlayingStatus.PLAY
+                    }
+                }
+                ACTION_STOP -> {
+                    if (mediaPlayer?.isPlaying == true && playingStatus == PlayingStatus.PLAY){
+                        mediaPlayer?.stop()
+                        mediaPlayer?.release()
+                        mediaPlayer = null
+                    }
+                }
+                else -> {
+
+                }
+            }
+
+        }
         // TODO
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onPrepared(mp: MediaPlayer) {
+        mp.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release()
     }
 }
 
@@ -92,14 +152,14 @@ fun setNextVolume(data:ByteArray){
     monitorCount++
 }
 
-fun startMonitoring(file: File?) {
+fun startPlaying(file: File?) {
     val intent = Intent(context, PlayingService::class.java)
     intent.putExtra(ACTION_NAME, ACTION_START)
-    intent.putExtra(PARAM_PATH, file)
+    intent.putExtra(PARAM_PATH, file?.absolutePath)
     context?.startService(intent)
 }
 
-fun stopMonitoring() {
+fun stopPlaying() {
     val intent = Intent(context, PlayingService::class.java)
     intent.putExtra(ACTION_NAME, ACTION_STOP)
     context?.startService(intent)
